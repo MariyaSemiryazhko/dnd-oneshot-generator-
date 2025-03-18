875c3a2c03b420e0d35c8d867418480f43843316
MariyaSemiryazhko <semiryazhkomasha@gmail.com>
2025-03-18 09:00:17 +0100

    basic js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+function GenerateOneshot(event) {
+    event.preventDefault();
+
+    new Typewriter('#ideas', {
+  strings: ['The ideas go there'],
+        autoStart: true,
+        delay: 1,
+});
+    
+}
+
+
+
+let formElement = document.getElementById("generator-form");
+formElement.addEventListener("submit", GenerateOneshot);
+
